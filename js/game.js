4c428e607828bb3d21901715f7a9c7018ca7d65a
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-11-05 20:08:28 +0200

    Update!

--- js/game.js
+++ js/game.js
@@ -7,9 +7,14 @@ ground.src = 'img/ground.png';
 const carrots = new Image();
 carrots.src = 'img/carrots.png';
 
+const keys = new Image();
+keys.src = 'img/keys.png';
+
 let box = 32;
 
 let score = 0;
+
+const control = 'control:';
 
 let food = {
   x: ~~(Math.random() * 17 + 1) * box,
@@ -41,6 +46,7 @@ function eatTail(head, arr) {
 
 function drawGame() {
   ctx.drawImage(ground, 0, 0);
+  ctx.drawImage(keys, 500, 10, 80, 80);
   ctx.drawImage(carrots, food.x, food.y);
 
   for (let i = 0; i < snake.length; i += 1) {
@@ -49,8 +55,9 @@ function drawGame() {
   }
 
   ctx.fillStyle = 'white';
-  ctx.font = '50px Arial';
+  ctx.font = '40px Arial';
   ctx.fillText(score, box * 2.5, box * 1.7);
+  ctx.fillText(control, 385, 50, 100);
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -66,7 +73,8 @@ function drawGame() {
   }
 
   if (snakeX < box || snakeX > box * 17 || snakeY < 3 * box || snakeY > box * 17)
-    clearInterval(game);
+    clearInterval(game), alert('Ви програли 😭!, щоб зіграти знову, оновіть сторінку!');
+
   if (dir == 'left') snakeX -= box;
   if (dir == 'right') snakeX += box;
   if (dir == 'up') snakeY -= box;
